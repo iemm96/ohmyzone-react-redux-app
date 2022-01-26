39d9a6f86cb787038fcb16f04973634cb5eaf3ca
Emmanuel <iemm96@gmail.com>
2022-01-25 22:08:45 -0600

    Styles tweaks

--- src/pages/Uploader.tsx
+++ src/pages/Uploader.tsx
@@ -1,13 +1,9 @@
-import React from 'react'
-import { Container } from '@mui/material';
 import UploadFile from '../components/UploadFile';
 
 const Uploader = () => {
     return (
         <>
-            <Container>
-                <UploadFile accept=".jpg, .jpeg, .png"/>
-            </Container>
+            <UploadFile accept=".jpg, .jpeg, .png"/>
         </>
     )
 }
