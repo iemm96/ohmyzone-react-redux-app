c82072a70caa59ad1da1c556d10a3da1c81b49f3
Emmanuel <iemm96@gmail.com>
2022-02-05 11:42:48 -0600

    Added PaletteType

---
+++ src/types/PaletteType.ts
@@ -0,0 +1,11 @@
+  
+type PaletteType = {
+    vibrant: string,
+    lightVibrant: string,
+    darkVibrant: string,
+    muted: string,
+    lightMuted: string,
+    darkMuted: string
+}
+
+export default PaletteType;
