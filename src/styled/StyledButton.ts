4f52208f2141c876dc58b5d70b583d6b60d75789
Emmanuel <iemm96@gmail.com>
2022-02-15 20:07:10 -0600

    Added StyledButton component

---
+++ src/styled/StyledButton.ts
@@ -0,0 +1,8 @@
+import { styled } from "@mui/material";
+import Button from '@mui/material/Button';
+
+const StyledButton = styled(Button)(() => ({
+    textTransform: 'none',
+}));
+
+export default StyledButton;
