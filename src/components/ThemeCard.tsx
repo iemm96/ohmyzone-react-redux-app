10c9667fba15ced8181ad02dcc18b6c98fbfd7cf
Emmanuel <iemm96@gmail.com>
2022-02-19 18:53:50 -0600

    Added ThemeCard

---
+++ src/components/ThemeCard.tsx
@@ -0,0 +1,54 @@
+import Card from '@mui/material/Card';
+import { CardActionArea } from '@mui/material';
+import CardMedia from '@mui/material/CardMedia';
+import {ColorPaletteImage} from "./ColorPaletteImage";
+import { useState } from 'react';
+
+
+type CurrentPaletteType = {
+    vibrant: string,
+    lightVibrant: string,
+    darkVibrant: string,
+    muted: string,
+    lightMuted: string,
+    darkMuted: string
+}
+
+
+const ThemeCard = ({ arrayRef, urlImage, darkMode, index, setImage, largeImageURL }:{ largeImageURL:string, arrayRef:any, urlImage:string, darkMode:boolean, index:number, setImage:any }) => {
+
+    const [currentPalette,setCurrentPalette] = useState<CurrentPaletteType>({
+        vibrant: '#4664F6',
+        lightVibrant: '#F8FAFF',
+        darkVibrant: '#010413',
+        muted: '#7AE7C7',
+        lightMuted: '#CBF6E9',
+        darkMuted: '#03110D'
+    });
+
+    const updateThemeColor = (index:any) => {
+        const reff:any = arrayRef[index];
+        reff?.current.updatePalette();
+        setImage( largeImageURL );
+    }
+
+    return(
+
+        <Card sx={{ maxWidth: 345, borderRadius: 4, backgroundColor: darkMode ? currentPalette.lightVibrant : currentPalette.darkVibrant }} key={index}>
+            <CardActionArea onClick={() => {
+                    updateThemeColor(index);
+                    
+                }}>
+                <CardMedia
+                    component="img"
+                    height="140"
+                    image={ urlImage }
+                    alt="image-alt"
+                />
+                <ColorPaletteImage setCurrentPalette={ setCurrentPalette } darkMode={darkMode} ref={arrayRef[index]} src={ urlImage }/>    
+            </CardActionArea>
+        </Card>
+    )
+}
+
+export default ThemeCard;
