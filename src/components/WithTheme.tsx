4e8509bc6bde87c29a682dea57942971d2a328f9
Emmanuel <iemm96@gmail.com>
2022-02-21 12:12:39 -0600

    WithTheme has a default theme and changes with state

--- src/components/WithTheme.tsx
+++ src/components/WithTheme.tsx
@@ -1,27 +1,83 @@
-import { useState } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { useSelector } from 'react-redux';
+import { PaletteMode } from '@mui/material';
 
+type DefaultThemeType = {
+    primaryMainLight: string;
+    primaryMainDark: string;
+    secondaryMain: string;
+    backgroundLight: string;
+    backgroundDark: string;
+    paperDark: string;
+    paperLight: string;
+}
 
 const Theme = (props:any) => {
-    const { primaryMain, secondaryMain } = useSelector( (state:any) => state.theme );
+    const { primaryMain, secondaryMain, background } = useSelector( (state:any) => state.theme );
 
     const { children } = props;
-    const [ darkMode,setDarkMode ] = useState<Boolean>(true);
+    const [ mode, setMode ] = useState<PaletteMode>( 'dark' );
+    const [ theme, setTheme ] = useState<any>(null);
+    const [ defaultTheme, setDefaultTheme ] = useState<DefaultThemeType>({
+        primaryMainLight: '#4664F6',
+        primaryMainDark: '#4664F6',
+        secondaryMain: '#141B41',
+        backgroundLight: '#F8FAFF',
+        backgroundDark: '#010413',
+        paperDark: '#030C3A',
+        paperLight: '#C5CEFC',
+    });
 
-    const theme = createTheme({
+    useEffect(() => {
+        setTheme(createTheme(getDesignTokens(mode)));
+    },[ primaryMain ]);
+
+
+    useEffect(() => {
+       
+
+        if( mode === 'light') {
+            document.body.style.backgroundColor = background ? background : defaultTheme.paperLight;
+        }else {
+            document.body.style.backgroundColor = background ? background : defaultTheme.paperDark;
+        }
+    },[])
+
+    const getDesignTokens = (mode: PaletteMode) => ({
         palette: {
-            mode: darkMode ? 'dark' : 'light',
-            primary: {
-                main: primaryMain ? primaryMain : '#F77F00',
-            },
-            secondary: {
-                main: secondaryMain ? secondaryMain : '#F8FAFF',
-            },
-        },
+            mode,
+            ...(mode === 'light'
+              ? {
+                    // palette values for light mode
+                    primary: {
+                        main: primaryMain ? primaryMain : defaultTheme.primaryMainLight
+                    },
+                    secondary: {
+                        main: secondaryMain ? secondaryMain : defaultTheme.secondaryMain,
+                    },
+                    background: {
+                        default: background ? background : defaultTheme.backgroundLight,
+                        paper: defaultTheme.paperLight,
+                    },
+                }
+              : {
+                    // palette values for light mode
+                    primary: {
+                        main: primaryMain ? primaryMain : defaultTheme.primaryMainDark
+                    },
+                    secondary: {
+                        main: secondaryMain ? secondaryMain : defaultTheme.secondaryMain,
+                    },
+                    background: {
+                        default: background ? background : defaultTheme.backgroundDark,
+                        paper: defaultTheme.paperDark,
+                    },
+                }),
+          },
     })
   
-    return <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    return <ThemeProvider theme={theme ? theme : createTheme(getDesignTokens(mode))}>{children}</ThemeProvider>
   }
   
   export default function withTheme(Component:any) {
