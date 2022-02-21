d1c028993c828f2eb9274681abb388cb56706713
Emmanuel <iemm96@gmail.com>
2022-02-21 12:10:14 -0600

    Added ZoneForm component

--- src/pages/zoneForm/ZoneForm.tsx
+++ src/pages/zoneForm/ZoneForm.tsx
@@ -1,5 +1,5 @@
 import withTheme from '../../components/WithTheme';
-import { Container, Paper } from '@mui/material';
+import { Container, Paper, Grid, useMediaQuery } from '@mui/material';
 import { useTheme } from '@mui/material';
 import { useParams } from 'react-router-dom';
 import PreviewSection from './PreviewSection';
@@ -7,17 +7,34 @@ import CoverSection from './CoverSection';
 import StepperComponent from '../../components/StepperComponent';
 import SocialIconsSection from './SocialIconsSection';
 import { LinksSection } from './LinksSection';
-import { useEffect } from 'react';
 import ThemeSection from './ThemeSection';
+import { useEffect } from 'react';
+import { fetchRecord } from '../../actions/fetchRecord';
+import { useDispatch } from 'react-redux';
+import { updateZone } from '../../actions/zones';
   
 const ZoneForm = () => {
     
     const params = useParams();
     const theme = useTheme();
+    const dispatch = useDispatch();
+    const mediaQuery = useMediaQuery(theme.breakpoints.up('sm'));
 
+    console.log(mediaQuery)
     useEffect(() => {
-        document.body.style.backgroundColor = theme.palette.secondary.main;
-    })
+        getZone();
+    },[])
+
+    const getZone = async () => {
+        if(params.zone) {
+            const { zone } = await fetchRecord('zones', params.zone);
+            dispatch( updateZone( {
+                ...zone,
+                profileImage: zone?.profileImage?.url
+            } ) );
+        }
+
+    }
 
     return (
         <> 
@@ -25,24 +42,35 @@ const ZoneForm = () => {
                 sx={{
                     pt: 10,
                     height: '100%',
-                    backgroundColor: theme.palette.secondary.main,
                     borderRadius: 0
                 }}
                 elevation={ 0 }
             >
-                <Container maxWidth="sm">
-                    <StepperComponent actualStep={ params.part } totalSteps={ 4 } />
+                <Container maxWidth="lg">
                     
-                    { params.part === '1' && <CoverSection/> }
                     
-                    { params.part === '2' && <SocialIconsSection prev={ 1 } next={ 3 }/> }
+                    <Grid container>
+                        <Grid md={ 6 } item>
+                            <StepperComponent actualStep={ params.part } totalSteps={ 4 } />
 
-                    { params.part === '3' && <LinksSection prev={ 2 } next={ 4 }/> }
+                            { params.part === '1' && <CoverSection/> }
+                        
+                            { params.part === '2' && <SocialIconsSection prev={ 1 } next={ 3 }/> }
 
-                    { params.part === '5' && <PreviewSection/> }
+                            { params.part === '3' && <LinksSection prev={ 2 } next={ 4 }/> }
+
+                            { params.part === '5' && <PreviewSection/> }
+
+                            { params.part === '4' && <ThemeSection prev={ 3 } next={ 5 }/> }
+                        </Grid>
+                        <Grid md={ 6 } item>
+                            <PreviewSection/>
+                        </Grid>
+                    </Grid>
+
                 </Container>
 
-                { params.part === '4' && <ThemeSection prev={ 3 } next={ 5 }/> }
+              
             </Paper>
         </>
     )
